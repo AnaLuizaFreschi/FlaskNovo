dbebb74347411e77ef44f8e7133f4cc78dad32e5
AnaLuizaFreschi <freschianaluiza@gmail.com>
2024-11-01 10:35:51 -0300

    Aula 01-03

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ from flask_migrate import Migrate
 db.init_app(app)
 migrate = Migrate(app, db)
 from models import Usuario, Pizza, Pedido
+from modulos.usuarios.usuarios import bp_usuario
+app.register_blueprint(bp_usuario, url_prefix='/usuarios')
+from modulos.pizzas.pizzas import bp_pizza
+app.register_blueprint(bp_pizza, url_prefix='/pizzas')
